44083975c1c75059288d95f869bb233b85316ab7
Insthync <ittipon.bay@gmail.com>
2021-02-15 11:27:53 +0700

    Add service result class

---
+++ Components/GoogleRewardedAdResult.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class GoogleRewardedAdResult : GameServiceResult
+{
+    public List<PlayerCurrency> updateCurrencies = new List<PlayerCurrency>();
+    public List<PlayerStamina> updateStaminas = new List<PlayerStamina>();
+}
